8b7661a5461e286764fbd59d4a44d23ef79a7769
freejoe76 <jmurphy@denverpost.com>
2017-08-01 16:36:12 -0400

    Fixes in the meta and paths

--- project/app.py
+++ project/app.py
@@ -18,8 +18,8 @@ page = {
     'description': '',
     'description_fb': '',
     'twitter': '',
-    'author': '',
-    'datestamp': '2017-07-',
+    'author': '"Interactive Project", "Kelli R. Parker", "Joe Murphy"',
+    'datestamp': '2017-08-02',
     'keywords': '',
     'keywords_array': '',
     'shareimg': '',
@@ -51,10 +51,10 @@ class JsonQuery(object):
 
 @app.route('/')
 def index():
-    app.page['url'] = 'amazing-history-nyc'
+    app.page['url'] = 'http://interactive.nydailynews.com/project/archive/amazing-history-nyc/'
     app.page['title'] = 'The amazing history of New York City'.decode('utf-8')
     app.page['description'] = 'Explore the sinners, saints, victors, victims, lovers, lost souls, magnates, madmen, geniuses and fools who powered the epic rise of NYC.'.decode('utf-8')
-    app.page['keywords'] = 'history of new York city, new york history, old new York, history of nyc, nyc history, history of new York, new York city history, manhattan history, jay maeder, big town'.decode('utf-8')
+    app.page['keywords'] = 'history of new york city, new york history, old new york, history of nyc, nyc history, history of new york, new york city history, manhattan history, jay maeder, big town'.decode('utf-8')
     app.page['title_fb'] = 'The amazing history of New York City'.decode('utf-8')
     app.page['description_fb'] = 'Explore the sinners, saints, victors, victims, lovers, lost souls, magnates, madmen, geniuses and fools who powered the epic rise of America’s largest metropolis.'.decode('utf-8')
     app.page['twitter'] = 'Explore the sinners, saints, geniuses and fools who powered the epic rise of NYC in this AMAZING history. http://nydn.us/historyofNYC'.decode('utf-8')
